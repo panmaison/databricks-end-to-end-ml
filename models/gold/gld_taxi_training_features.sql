ba23a339593c612b5a8fdad1dcf2b2fbc73ccccd
Hongwei Pan <Panmaison@gmail.com>
2026-04-02 13:52:00 +0000

    add pickup time

--- models/gold/gld_taxi_training_features.sql
+++ models/gold/gld_taxi_training_features.sql
@@ -1,17 +1,28 @@
 select
+    trip_id,
     vendor_name,
     passenger_count,
     trip_distance,
     rate_code,
-    pickup_location_id,
-    dropoff_location_id,
     payment_type,
-    pickup_hour,
-    pickup_day_of_week,
-    pickup_month,
-    pickup_is_weekend,
     fare_amount,
     tip_amount,
     total_amount,
+    surcharge,
+    mta_tax,
+    tolls_amount,
+    start_lon,
+    start_lat,
+    end_lon,
+    end_lat,
+    pickup_datetime,
+    hour(pickup_datetime) as pickup_hour,
+    dayofweek(pickup_datetime) as pickup_day_of_week,
+    month(pickup_datetime) as pickup_month,
+    case
+        when dayofweek(pickup_datetime) in (1, 7) then 1
+        else 0
+    end as pickup_is_weekend,
+    avg_speed_mph,
     trip_duration_minutes as label
 from {{ ref('slv_taxi_time_features') }}
--- models/silver/slv_taxi_time_features.sql
+++ models/silver/slv_taxi_time_features.sql
@@ -1,10 +1,44 @@
-select
-    *,
-    hour(pickup_datetime) as pickup_hour,
-    dayofweek(pickup_datetime) as pickup_day_of_week,
-    month(pickup_datetime) as pickup_month,
-    case
-        when dayofweek(pickup_datetime) in (1, 7) then 1
-        else 0
-    end as pickup_is_weekend
-from {{ ref('slv_taxi_trips_clean') }}
+with base as (
+
+    select
+        *,
+        md5(concat_ws(
+        '||',
+        coalesce(cast(vendor_name as string), ''),
+        coalesce(cast(pickup_datetime as string), ''),
+        coalesce(cast(dropoff_datetime as string), ''),
+        coalesce(cast(passenger_count as string), ''),
+        coalesce(cast(trip_distance as string), ''),
+        coalesce(cast(rate_code as string), ''),
+        coalesce(cast(payment_type as string), ''),
+        coalesce(cast(fare_amount as string), ''),
+        coalesce(cast(tip_amount as string), ''),
+        coalesce(cast(total_amount as string), ''),
+        coalesce(cast(surcharge as string), ''),
+        coalesce(cast(mta_tax as string), ''),
+        coalesce(cast(tolls_amount as string), ''),
+        coalesce(cast(start_lon as string), ''),
+        coalesce(cast(start_lat as string), ''),
+        coalesce(cast(end_lon as string), ''),
+        coalesce(cast(end_lat as string), ''),
+        coalesce(cast(store_and_forward as string), '')
+    )) as trip_id,
+        hour(pickup_datetime) as pickup_hour,
+        dayofweek(pickup_datetime) as pickup_day_of_week,
+        month(pickup_datetime) as pickup_month,
+        case
+            when dayofweek(pickup_datetime) in (1, 7) then 1
+            else 0
+        end as pickup_is_weekend
+    from {{ ref('slv_taxi_trips_clean') }}
+),
+
+dedup as (
+
+    select distinct *
+    from base
+
+)
+
+select *
+from dedup
